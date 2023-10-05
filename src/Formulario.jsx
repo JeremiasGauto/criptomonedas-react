89dd45552f3c548010ee32d625ef935cd1a1bec6
Jeremias Gauto <jeremiasg_kc@hotmail.com>
2023-10-05 12:37:46 -0300

    input submit listo

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -10,20 +10,27 @@ const InputSubmit = styled.input`
     text-transform: uppercase;
     font-size: 20px;
     border-radius: 5px;
+    transition: background-color .3 ease;
+
+    &:hover{
+        background-color: #7a7dfe;
+        cursor: pointer;
+
+    }
 `
 
 function Formulario() {
     return (
-      
-     <form>
+
+        <form>
             <InputSubmit
                 type="submit"
                 value="Cotizar"
-            />    
+            />
 
 
-    </form>
-  )
+        </form>
+    )
 }
 
 export default Formulario
